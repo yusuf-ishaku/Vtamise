cb0f1abef7fb6e6922482aa504f9aa0750939613
spolonko <kingshakzy@gmail.com>
2023-01-26 15:48:10 +0100

    chat app started

--- chatapp.js
+++ chatapp.js
@@ -1,1 +1,6 @@
- 
+
+const mailer = document.querySelector("#chat-submit");
+mailer.addEventListener("click", () =>{
+    let _chat = document.querySelector("#message");
+    console.log(_chat.value)
+} )
